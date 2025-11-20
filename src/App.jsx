252bcc99de7f425c11a534207bd16dd73a0cde2c
nawaraj591 <nawanitric@gmail.com>
2025-11-20 21:30:46 +0545

    Added new folder

---
+++ src/App.jsx
@@ -0,0 +1,369 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+// const App = () => {
+//   console.log('Hello from component')
+//   return (
+//     <div>
+//       <p>Hello world</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   console.log(now, a+b)
+
+//   return (
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+// export default App
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   return React.createElement(
+//     'div',
+//     null,
+//     React.createElement(
+//       'p', null, 'Hello world, it is ', now.toString()
+//     ),
+//     React.createElement(
+//       'p', null, a, ' plus ', b, ' is ', a + b
+//     )
+//   )
+// }
+
+// export default App
+
+// const Hello = () => {
+//   return (
+//     <div>
+//       <p>Hello world</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+
+//       <Hello />
+//     </div>
+//   )
+// }
+
+// export default App
+
+// const Hello = (props) => {
+
+//   console.log(props)
+//   return (
+//     <div>
+//       <p>
+
+//         Hello {props.name}, you are {props.age} years old
+//       </p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+
+//       <Hello name='Maya' age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// const App = () => {
+//   const friends = [
+//     { name: 'Peter', age: 4 },
+//     { name: 'Maya', age: 10 },
+//   ]
+
+//   return (
+//     <div>
+//       <p>{friends[0].name} {friends[0].age}</p>
+//       <p>{friends[1].name} {friends[1].age}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// const Hello = (props) => {
+
+//   const bornYear = () => {
+//     const yearNow = new Date().getFullYear()
+//     return yearNow - props.age
+//   }
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {props.name}, you are {props.age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// const History = (props) => {
+//   if (props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+
+
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setLeft(left + 1)
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+//   }
+
+//   return (
+//     <div>
+//       {left}
+
+//       <Button onClick={handleLeftClick} text='left' />
+//       <Button onClick={handleRightClick} text='right' />
+//       {right}
+//       <History allClicks={allClicks} />
+//     </div>
+//   )
+// }
+
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+//   const [total, setTotal] = useState(0)
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setLeft(left + 1)
+
+//     setTotal(left + right)
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+
+//     setTotal(left + right)
+//   }
+
+//   return (
+//     <div>
+//       {left}
+//       <button onClick={handleLeftClick}>left</button>
+//       <button onClick={handleRightClick}>right</button>
+//       {right}
+//       <p>{allClicks.join(' ')}</p>
+
+//       <p>total {total}</p>
+//     </div>
+//   )
+// }
+// export default App
+
+// const Display = props => <div>{props.value}</div>
+
+// const Button = (props) => (
+//   <button onClick={props.onClick}>
+//     {props.text}
+//   </button>
+// )
+
+// const App = () => {
+//   const [value, setValue] = useState(10)
+
+//   const setToValue = newValue => {
+//     console.log('value now', newValue)
+//     setValue(newValue)
+//   }
+
+//   return (
+//     <div>
+//       <Display value={value} />
+//       <Button onClick={() => setToValue(1000)} text="thousand" />
+//       <Button onClick={() => setToValue(0)} text="reset" />
+//       <Button onClick={() => setToValue(value + 1)} text="increment" />
+//     </div>
+//   )
+// }
+// export default App
+
+
+// const App = () => {
+//     console.log('Hello from component')
+//   return (
+//     <div>
+//       <p>Hello world</p>
+//     </div>
+//   )
+// }
+
+// const Hello = (props) => {
+//     console.log(props)
+//     return (
+//         <div>
+//             <p>Hello {props.name}, you are {props.age} years old                
+//             </p>
+//         </div>
+//     )
+// }
+
+// const App = () => {
+//     const friends = [
+//         {name: 'Peter', age:4 },
+//         {name: 'Maya', age:10},
+//     ]
+    
+//     return (
+//         <div>
+//             <p>{friends[0].name} {friends[0].age}</p>
+//             <p>{friends[1].name} {friends[1].age}</p>                
+//         </div>
+//     )
+// }
+
+// const App = () => {
+//   const now = new Date()
+//   const a = 10
+//   const b = 20
+//   console.log(now, a+b)
+
+//   return (
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // const-definitions
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content ... />
+      <Total ... />
+    </div>
+  )
+}
+
+
+export default App
